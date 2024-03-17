4cdd945b6084cfe59bd979bbbfc103b4a3b2c3cc
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2024-03-18 01:50:19 +0530

    Changes made in collison file after a very long time

--- rrt_assn/detection.py
+++ rrt_assn/detection.py
@@ -11,26 +11,45 @@ class Node():
         self.parent_x = []
         self.parent_y = []
 
-def collision(x1,y1,x2,y2,box):
-    
-    points = list((np.arange(x1,x2,(x2-x1)/9),np.arange(y1,y2,(y2-y1)/9)))
-    print(points)
-    for p in points:
-        
-        if (np.isin(p[0],box[0]).any()) and (np.isin(p[1],box[1]).any()) :
-            return True
-        else: return False
+def collision(x1,y1,x2,y2):
+    color=[]
+    x = list(np.arange(x1,x2,(x2-x1)/100))
+    y = []
+    for x_cord in x:
+        y.append(((y2-y1)/(x2-x1))*(x_cord-x1) + y1)
+    cv2.line(sampling_image,(x1,y1),(x2,y2),(0,255,0),2)
+    for i in range(len(x)):
+        color.append(sampling_image[int(y[i]),int(x[i])])
+
+
+    # print(color)
+
+    color = color.flatten()    
+
+
+
+    # if (np.isin()) :
+    #     return True
+    # else: return False
 
 box = list((np.arange(1,2,1/9),np.arange(2,1.5,1/9))) + list((np.arange(2,3,1/9),np.arange(1.5,3.5,1/9)))+list((np.arange(3,0,1/9),np.arange(3.5,3,1/9))) + list((np.arange(0,1,1/9),np.arange(3,2,1/9)))
-print(collision(2,4,1,2.5,box))
 
 
-frame = cv2.imread("/home/dhruv/Desktop/MRT/rrt/src/rrt_assn/rrt_assn/arucoMarker.jpg")
+frame = cv2.imread("/home/dhruv/Desktop/MRT/rrt/src/rrt_assn/rrt_assn/arucoMarker.jpg") #(1080,1920,3)
+
+sampling_image = np.full((len(frame),len(frame[0])),255.0,dtype=np.uint8)
+# print(sampling_image.shape)
+sampling_image = cv2.cvtColor(sampling_image,cv2.COLOR_GRAY2BGR)
 gray_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+
+
+
 arucoDict = cv2.aruco.getPredefinedDictionary(aruco.DICT_4X4_100)
 arucoParams = cv2.aruco.DetectorParameters()
 detectors = cv2.aruco.ArucoDetector(arucoDict,arucoParams)
 corners, ids, rejectedMarkers = detectors.detectMarkers(gray_img)
+
 
 if len(corners)>0:
     ids = ids.flatten()
@@ -42,12 +61,27 @@ if len(corners)>0:
         topRight=(int(tr[0]),int(tr[1]))
         bottomRight=(int(br[0]),int(br[1]))
         bottomLeft=(int(bl[0]),int(bl[1]))
+        # for frame or orignal image
         cv2.line(frame,topLeft,topRight,(0,0,255),2)
         cv2.line(frame,topRight,bottomRight,(0,0,255),2)
         cv2.line(frame,bottomRight,bottomLeft,(0,0,255),2)
         cv2.line(frame,bottomLeft,topLeft,(0,0,255),2)
 
-cv2.imshow("markers",frame)
+        # for sampled image 
+        cv2.line(sampling_image,topLeft,topRight,0,2)
+        cv2.line(sampling_image,topRight,bottomRight,0,2)
+        cv2.line(sampling_image,bottomRight,bottomLeft,0,2)
+        cv2.line(sampling_image,bottomLeft,topLeft,0,2)
+
+
+print(collision(348,408,352,463))
+cv2.imshow("Sampling Image",sampling_image)
+
+
+# print(sampling_image[1425][503])
+# print(gray_img)
+
+# cv2.imshow("markers",frame)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
